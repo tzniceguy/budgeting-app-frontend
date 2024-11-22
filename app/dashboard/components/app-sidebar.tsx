a70dc2c2fdf8afb05e295f2ecb77c52a9d14f564
tzniceguy <joeshimbi@gmail.com>
2024-11-22 10:23:10 +0300

    sidebar in layout

---
+++ app/dashboard/components/app-sidebar.tsx
@@ -0,0 +1,67 @@
+import { ArrowRightLeftIcon, Goal, LayoutDashboard, PieChart,  Settings } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Transactions",
+    url: "#",
+    icon: ArrowRightLeftIcon,
+  },
+  {
+    title: "My Goals",
+    url: "#",
+    icon: Goal,
+  },
+  {
+    title: "Analytics and Reports",
+    url: "#",
+    icon: PieChart,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
